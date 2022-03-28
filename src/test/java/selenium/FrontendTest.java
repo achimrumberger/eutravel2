e3234d19f0693a3afe789b893ab91c873bb84403
Achim.Rumberger <achimrumberger@icloud.com>
2022-03-28 10:14:51 +0200

    -madded Selenium test

---
+++ src/test/java/selenium/FrontendTest.java
@@ -0,0 +1,89 @@
+package selenium;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class FrontendTest {
+	
+	@Test
+	public void testSearch() {
+		WebDriverManager.firefoxdriver().setup();
+		FirefoxOptions ffo = new FirefoxOptions();
+		//fission.webContentIsolationStrategy"to 0 and "fission.bfcacheInParent" to false
+		ffo.addArguments("--no-sandbox");
+		ffo.addArguments("--enable-javascript");
+
+		ffo.addArguments("--headless");
+		ffo.addPreference("fission.webContentIsolationStrategy", 0);
+		ffo.addPreference("fission.bfcacheInParent", false);
+
+		WebDriver driver = new FirefoxDriver(ffo);
+		JavascriptExecutor js = (JavascriptExecutor)driver;	
+		//driver.get("https://ikman.lk");
+		//URL launch
+		driver.get("http://localhost:4200");
+		WebElement originput =    driver.findElement(By.name("originput"));
+
+		WebElement destinput =    driver.findElement(By.name("destinput"));
+		WebElement btnsearch =    driver.findElement(By.name("btnsearch"));
+		WebElement result =    driver.findElement(By.name("result"));
+		WebElement inputDate = driver.findElement(By.name("inputdate"));
+		WebElement inputTime = driver.findElement(By.name("inputtime"));
+
+		originput.sendKeys("Berlin Hbf");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		waitUntilVisible(inputDate, driver);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		//how to interact with date picker???
+		//important: correct format for date
+		inputDate.sendKeys("Sat Mar 26 2022 00:00:00 GMT+0");
+		inputTime.sendKeys("13:00");
+		destinput.sendKeys("Stuttgart Hbf");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		waitUntilVisible(btnsearch, driver);
+		btnsearch.click();
+		waitUntilVisible(result, driver);
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		List<WebElement> listRows = driver.findElements(By.xpath("//*[@name='result']/tbody/tr"));
+		System.out.println(listRows);
+		for(WebElement tr: listRows) {
+			WebElement td = tr.findElement(By.xpath("./child::*"));
+			System.out.println(td.getText());
+			WebElement ahref = td.findElement(By.xpath("./child::*"));
+			System.out.println(ahref.getAttribute("href"));
+		}
+		assertEquals(listRows.size(), 3);
+
+		driver.close();
+		
+	}
+	
+	private static void waitUntilVisible(WebElement element,WebDriver driver) {
+		waitForCondition(ExpectedConditions.visibilityOf(element), Duration.ofSeconds(10), driver);
+	}
+
+	private static void waitForCondition(ExpectedCondition<WebElement> condition, Duration timeout, WebDriver driver) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		wait.until(condition);
+	}
+
+}
