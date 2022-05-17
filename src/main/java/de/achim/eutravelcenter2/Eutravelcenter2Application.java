59b72e3824ea8b949956e89e7a1a302619a96ca7
Achim.Rumberger <achimrumberger@icloud.com>
2022-05-17 11:21:34 +0200

    usage of graalvm

--- src/main/java/de/achim/eutravelcenter2/Eutravelcenter2Application.java
+++ src/main/java/de/achim/eutravelcenter2/Eutravelcenter2Application.java
@@ -2,8 +2,11 @@ package de.achim.eutravelcenter2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
+@TypeHint(typeNames = {"org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl",
+		"org.hibernate.boot.model.naming.ImplicitNamingStrategyLegacyJpaImpl"})
 public class Eutravelcenter2Application {
 
 	public static void main(String[] args) {
